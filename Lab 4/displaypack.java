7e82c8937f59435faaa49a5aaba7a0e3a30563e7
Tarun Joseph <tarunjoseph94@gmail.com>
2018-09-25 11:42:51 +0530

    Add files via upload

---
+++ Lab 4/displaypack.java
@@ -0,0 +1,13 @@
+package display;
+//      ^^^^^^^^This is the package name
+public class displaypack
+//^^^this has to be public
+
+{
+	public void show()
+//   ^^^this has to be public
+	{
+		System.out.println("Display");
+	}
+}
+//use javac -d . classname.java to compile package
---
+++ Lab 4/packdisp.java
@@ -0,0 +1,11 @@
+import display.displaypack;
+//     package name.classname
+class packdisp
+{
+public static void main(String[] args) {
+	displaypack d1= new displaypack();
+    // class is from package
+	d1.show();
+	
+}
+}
